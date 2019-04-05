7eb9f22cb308b390d9aa0da25c93e0141153cd4e
supjain <supjain@ebay.com>
2019-04-04 22:25:44 -0700

    Added layouts for displaying list items

---
+++ src/main/java/com/example/supjain/shoppinglist/DisplayShoppingListActivity.java
@@ -0,0 +1,14 @@
+package com.example.supjain.shoppinglist;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DisplayShoppingListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_shopping_list);
+    }
+}
